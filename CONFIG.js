41cbf3e25f049d0e8b19a6db488f1d153b320036
TranTrongNghia3008 <95363914+TranTrongNghia3008@users.noreply.github.com>
2022-12-19 02:04:45 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Anh biết là em sẽ hog giận anh nữa mà ^^ </br> Cám ơn em đã tha thứ, thương lắm á <3 .Đối với anh, em mãi là cô bé :3 </br> "Thích bé nhiều đến mấy, mong em sẽ hiểu. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Anh biết là em sẽ hog giận anh nữa mà ^^ </br> Cám ơn em đã tha thứ, thương lắm á <3 .Đối với anh, em mãi là cô bé :3 </br> "Thích bé nhiều đến mấy, mong em sẽ hiểu."',
 };
