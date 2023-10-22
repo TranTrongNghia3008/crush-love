84000499a80eb21e3e4686758063ca321eeef3dd
TranTrongNghia3008 <95363914+TranTrongNghia3008@users.noreply.github.com>
2023-10-22 20:59:05 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Đừng giận anh nữa nhé",
+  titleHeader: "Tha thứ cho anh nhá!",
   descriptionHeader:
     "Nếu bé thoát ra và không trả lời thì có nghĩa là bé đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Anh biết là em sẽ hog giận anh nữa mà ^^ </br> Cám ơn em đã tha thứ, thương lắm á <3 .Đối với anh, em mãi là cô bé :3 </br> "Thích bé nhiều đến mấy, mong em sẽ hiểu."',
+    'Anh biết là em sẽ hog giận anh nữa mà ^^',
 };
