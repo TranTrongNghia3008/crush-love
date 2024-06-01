2179d00770dbef9f4b77059e9b40df79a6153bdb
TranTrongNghia3008 <95363914+TranTrongNghia3008@users.noreply.github.com>
2024-06-01 22:36:00 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Hog cóa buồn nữa nha!",
   descriptionHeader:
-    "Nếu bé Di thoát ra thì có nghĩa là bé Di đã hết buồn rồi nhó ^^",
+    "Nếu bé Di thoát ra thì có nghĩa là bé Di đã hết buồn rồi nhó!^^",
   buttonYes: "Gòi gòi, hết bùn gòi",
   buttonNo: "Hog! Bùn quài luôn",
   titleModar: "Nhớ là đồng ý hết bùn gòi đóa",
